92b2e29fb8b0cb4782744c8a4b1b82c1ef092de0
uku <uku3lig@gmail.com>
2022-12-25 15:29:18 +0100

    rename mod

---
+++ src/main/java/net/uku3lig/chathighlighter/ChatHighlighter.java
@@ -0,0 +1,13 @@
+package net.uku3lig.chathighlighter;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class ChatHighlighter implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // :true:
+    }
+}
