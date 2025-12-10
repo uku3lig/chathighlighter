e23db10f5d26a780dacbf8a425d24d09b1bd7db3
uku <hi@uku.moe>
2025-12-10 08:20:09 +0100

    feat: update to 1.21.11

--- src/main/java/net/uku3lig/chathighlighter/mixin/MixinChatHud.java
+++ src/main/java/net/uku3lig/chathighlighter/mixin/MixinChatHud.java
@@ -1,9 +1,10 @@
 package net.uku3lig.chathighlighter.mixin;
 
-import lombok.extern.slf4j.Slf4j;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
-import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.ChatHud;
 import net.minecraft.client.gui.hud.ChatHudLine;
 import net.minecraft.client.network.ClientPlayerEntity;
@@ -14,7 +15,6 @@ import net.uku3lig.chathighlighter.ChatHighlighter;
 import net.uku3lig.chathighlighter.config.ChatHighlighterConfig;
 import net.uku3lig.ukulib.utils.Ukutils;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -24,20 +24,18 @@ import java.util.*;
 import java.util.regex.Matcher;
 
 @Mixin(ChatHud.class)
-@Slf4j
 public abstract class MixinChatHud {
     // fucky fix for chat patches compat, don't like it, but it is how it is
     @Unique
     private static final Set<Integer> pingedTicks = new HashSet<>();
 
-    @Shadow
-    protected abstract int getLineHeight();
+    @WrapOperation(method = "method_75802", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/ChatHud$Backend;fill(IIIII)V", ordinal = 0))
+    private static void highlight(ChatHud.Backend instance, int x1, int y1, int x2, int y2, int color, Operation<Void> original, @Local(argsOnly = true) ChatHudLine.Visible line, @Local(argsOnly = true, ordinal = 1) float opacity) {
+        original.call(instance, x1, y1, x2, y2, color);
 
-    @Inject(method = "method_71992", at = @At(value = "INVOKE", shift = At.Shift.AFTER, target = "Lnet/minecraft/client/gui/DrawContext;fill(IIIII)V", ordinal = 0))
-    public void highlight(DrawContext context, int i, float f, float g, int j, int k, /**/ int x, int y, int n, ChatHudLine.Visible message, int o, float opacity, CallbackInfo ci) {
         final ChatHighlighterConfig config = ChatHighlighter.getManager().getConfig();
         final TextRenderer textRenderer = MinecraftClient.getInstance().textRenderer;
-        final String str = Ukutils.getText(message.content()).toLowerCase(Locale.ROOT);
+        final String str = Ukutils.getText(line.content()).toLowerCase(Locale.ROOT);
 
         int highlightColor = ColorHelper.withAlpha((int) ((config.getColor() >> 24) * opacity), config.getColor() & 0xFFFFFF);
 
@@ -47,7 +45,7 @@ public abstract class MixinChatHud {
                 String before = str.substring(0, matcher.start());
                 int beforeWidth = textRenderer.getWidth(before) + ChatHighlighter.getOffset();
                 int width = textRenderer.getWidth(matcher.group());
-                context.fill(beforeWidth, y, width + beforeWidth, y + getLineHeight(), highlightColor);
+                instance.fill(beforeWidth, y1, width + beforeWidth, y2, highlightColor);
             }
         } else {
             for (String keyword : config.getText()) {
@@ -57,7 +55,7 @@ public abstract class MixinChatHud {
                     String before = str.substring(0, index);
                     int beforeWidth = textRenderer.getWidth(before) + ChatHighlighter.getOffset();
                     int width = textRenderer.getWidth(keyword);
-                    context.fill(beforeWidth, y, width + beforeWidth, y + getLineHeight(), highlightColor);
+                    instance.fill(beforeWidth, y1, width + beforeWidth, y2, highlightColor);
                     index = str.indexOf(keyword, index + 1);
                 }
             }
